6e89cb48e1bb55d0426a272aeff1d237849b5c17
Javid <108080942+JavidGull@users.noreply.github.com>
2025-03-02 00:02:33 +0000

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -4,7 +4,7 @@ sudo apt-get update && sudo apt-get install -y apache2
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 python3 -m pip install -U pip --break-system-packages
-sudo pip3 install -y azure-storage-blob azure-storage-file-share azure-storage-file-datalake azure-storage-queue --break-system-packages
-pip3 install -y configparser
-sudo pip install -y --ignore-installed Flask --break-system-packages
-sudo mkdir -p /var/www/html/upload && cp ./templates/* /var/www/html/upload/
+sudo pip3 install azure-storage-blob azure-storage-file-share azure-storage-file-datalake azure-storage-queue --break-system-packages
+pip3 install configparser
+sudo pip install --ignore-installed Flask --break-system-packages
+sudo mkdir -p /var/www/html/upload && sudo cp ./templates/* /var/www/html/upload/
