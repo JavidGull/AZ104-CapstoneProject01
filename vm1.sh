6fa93ff1f9c668749e11dd66473ade60e7092a1b
Javid <108080942+JavidGull@users.noreply.github.com>
2025-03-04 12:57:04 +0000

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo ufw allow 5000/tcp
-sudo apt-get update && sudo apt-get install -y apache2
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 python3 -m pip install -U pip --break-system-packages
