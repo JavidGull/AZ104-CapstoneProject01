f64d0a6004d627d62426d244d63576676dd73e81
Javid <108080942+JavidGull@users.noreply.github.com>
2025-03-04 10:47:14 +0000

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -7,5 +7,6 @@ python3 -m pip install -U pip --break-system-packages
 pip3 install azure-storage-blob azure-storage-file-share azure-storage-file-datalake azure-storage-queue --break-system-packages
 pip3 install configparser --break-system-packages
 pip install --ignore-installed Flask --break-system-packages
+sudo cp *.py /var/www/html/
 sudo mkdir -p /var/www/html/upload && sudo cp ./templates/* /var/www/html/upload/
 export PATH="/home/ubuntu/.local/bin:$PATH"
