b6064908fd7ec89825a61ddfb933753ced4d08ee
Javid <108080942+JavidGull@users.noreply.github.com>
2025-03-04 14:26:04 +0000

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -3,8 +3,8 @@ sudo ufw allow 5000/tcp
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 python3 -m pip install -U pip --break-system-packages
-pip3 install azure-storage-blob azure-storage-file-share azure-storage-file-datalake azure-storage-queue --break-system-packages
+sudo pip3 install azure-storage-blob azure-storage-file-share azure-storage-file-datalake azure-storage-queue --break-system-packages
 pip3 install configparser --break-system-packages
-#pip install --ignore-installed Flask --break-system-packages
-sudo apt install python3-flask
+#sudo pip3 install --ignore-installed Flask --break-system-packages
+sudo apt install -y python3-flask
 export PATH=$PATH:/home/ubuntu/.local/bin
